8912762219c1788943961edd2003b6c78eaf4546
DYSTOpyy <bcd.saltel@gmail.com>
2026-01-12 22:39:03 +0100

    fix(layout): simplify image positioning using Tailwind CSS classes

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -2,16 +2,11 @@ import styles from "./app.module.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={"relative w-screen overflow-clip"}>
+    <div className="relative w-screen overflow-clip">
       <img
         src="/svg/vagues.svg"
         alt="Rotated Image"
-        style={{
-          position: "fixed",
-          left: "-300px",
-          top: "-100px",
-          zIndex: 10,
-        }}
+        className="fixed -left-[300px] -top-[100px] z-0"
       />
       {children}
     </div>
