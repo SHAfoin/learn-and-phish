5bf290ba6b37f0e5ff4c24f068c82578136536c9
DYSTOpyy <bcd.saltel@gmail.com>
2026-01-12 22:38:15 +0100

    feat(layout): enhance layout component with background image and children

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,3 +1,19 @@
+import styles from "./app.module.css";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className={"relative w-screen overflow-clip"}>
+      <img
+        src="/svg/vagues.svg"
+        alt="Rotated Image"
+        style={{
+          position: "fixed",
+          left: "-300px",
+          top: "-100px",
+          zIndex: 10,
+        }}
+      />
+      {children}
+    </div>
+  );
 }
